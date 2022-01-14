062d5fbea970e102b10f6124169522489cdd8761
Lia Moua <moua0061@yahoo.com>
2022-01-14 12:26:00 -0500

    starting again

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,30 @@
-// add middlewares here related to projects
+const Project = require('./projects-model');
+
+function idIsValid(req, res, next) {
+    //returns a valid id 
+    Project.get(req.params.id)
+        .then(something => {
+            console.log(something)
+            if(!something) {
+                res.status(404).json({
+                    message: 'user does not exist'
+                })
+            } else {
+                req.user = something
+                console.log(something)
+                next()
+            }
+        })
+        .catch(err => {
+            next(err)
+        })
+}
+
+function secondMiddlware(req, res, next) {
+    //returns a valid body 
+}
+
+module.exports = {
+    idIsValid,
+    secondMiddlware
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,12 +1,21 @@
 const express = require('express');
-const server = require('../server');
 const Project = require('./projects-model');
 //import middleware here
 
 const router = express.Router();
 
 //GET/api/projects
+router.get('/', (req, res, next) => {
+    //returns an array of all the resources contained in the DB
+})
 
 //import error middelware here
+router.use((err, req, res, next) => {
+    console.log('YO! something went wrong really bad!')
+    res.status(err.status || 500).json({
+        message: 'this is the super bad path',
+        error: err.message
+    })
+});
 
 module.exports = router;
